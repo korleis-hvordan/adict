1fe2958744c7fe947407784108d42ea505ba1906
Alexander E. Dang <alex.dang3606@gmail.com>
2023-10-21 23:21:09 +0700

    initial upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,25 @@ import { useState } from "react";
 
 import "./App.css";
 
+const thinSpace = String.fromCharCode(8201);
+const accent = String.fromCharCode(0x0301);
+const tie = String.fromCharCode(865);
+
+async function getTextFromStream(readableStream) {
+  let reader = readableStream.getReader();
+  let decoder = new TextDecoder("ISO-8859-1");
+  let nextChunk;
+  
+  let resultStr = '';
+  
+  while (!(nextChunk = await reader.read()).done) {
+      let partialData = nextChunk.value;
+      resultStr += decoder.decode(partialData);
+  }
+  
+  return resultStr;
+}
+
 function App() {
   const [field, setField] = useState("");
 
@@ -17,7 +36,7 @@ function App() {
     const htmlElem = document.createElement("html");
     htmlElem.innerHTML = html;
     return [...htmlElem.querySelectorAll("table.res td:has(> a)")]
-    .map(e => e.innerText.substring(0, e.innerText.length - 2));
+    .map(e => e.innerText.substring(0, e.innerText.length - 2).toLowerCase());
   }
 
   return (
@@ -25,34 +44,58 @@ function App() {
       <form onSubmit={async e => {
         e.preventDefault();
 
-        const [res1, res2, res3] = await Promise.all([
-          fetch(`https://cors-anywhere.herokuapp.com/http://seas.elte.hu/cube/index.pl?s=${field}&fullw=on&invr=on`),
-          fetch(`https://cors-anywhere.herokuapp.com/http://seas.elte.hu/cube/index.pl?s=${field}&fullw=on&invr=on&asp=on`),
-          fetch(`https://cors-anywhere.herokuapp.com/http://seas.elte.hu/cube/index.pl?s=${field}&fullw=on&invr=on&thop=on`)
-        ]);
-        const [html1, html2, html3] = await Promise.all([
-          res1.text(), res2.text(), res3.text()
+        setResults([]);
+
+        const [res1, res2] = await Promise.all([
+          fetch(`http://seas.elte.hu/cube/index.pl?s=${escape(field)}&fullw=on&invr=on&goal=on`),
+          fetch(`http://seas.elte.hu/cube/index.pl?s=${escape(field)}&fullw=on&invr=on&goal=on&thop=on`)
         ]);
 
-        let [none, asp, thop] = [getIPAs(html1), getIPAs(html2), getIPAs(html3)];
-        none.forEach((e, i) => {
+        const [html1, html2] = await Promise.all([
+          getTextFromStream(res1.body), getTextFromStream(res2.body)
+        ]);
+
+        let [none, thop] = [getIPAs(html1), getIPAs(html2)];
+
+        if (none.length === 0) {
+          setResults(["no results"]);
+          return;
+        }
+
+        thop.forEach((e, i) => {
           [...e].forEach((c, index) => {
-            if (c !== asp[i][index]) {
-              thop[i] = thop[i].substring(0, index) + "t" + thop[i].substring(index + 1);
+            if (c === "ʰ") {
+              none[i] = none[i].substring(0, index + 1) + "ʰ" + thinSpace
+                + none[i].substring(index + 1);
             }
           });
         });
 
-        const wordList = getWords(html3);
-        setResults(thop.filter((e, i) => wordList[i] === field.trim()));
+        const wordList = getWords(html1);
+        none = none.filter((e, i) => wordList[i] === field.trim().toLowerCase());
+
+        none = none.map(e => {
+          return e.split(thinSpace).map(ele => {
+            if (ele.length > 1) {
+              if (ele.length == 2 && ele[1] !== accent && ele[1] !== "ː") {
+                return ele[0] + tie + thinSpace + ele[1];
+              }
+              else if (ele.length == 3 && ele[2] !== "ː") {
+                return ele.substring(0, 2) + tie + thinSpace + ele[2];
+              }
+            }
+            return ele;
+          }).join(thinSpace);
+        });
+
+        setResults(none.length ? [...new Set(none)] : ["no results"]);
       }}>
         <input type="search" value={field} onChange={e => setField(e.target.value)} />
         <input type="submit" value="search" />
-        <input type="checkbox" />multiple words (slow)
       </form>
       <ul>
-        {results.map(e => <li className="ipa">{e}</li>)}
-      </ul>
+        {results.map((e, i) => <li className="ipa" key={i}>{e}</li>)}
+      </ul> 
     </div>
   );
 }
