f8c2008c72d3e0601776f2f1357cd0e30f996432
Alexander E. Dang <alex.dang3606@gmail.com>
2023-10-28 09:23:31 +0700

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "./App.css";
 
@@ -23,7 +23,7 @@ async function getTextFromStream(readableStream) {
 
 function App() {
   const [field, setField] = useState("");
-
+  const [lastWord, setLastWord] = useState("");
   const [results, setResults] = useState([]);
 
   function getIPAs(html) {
@@ -39,55 +39,60 @@ function App() {
     .map(e => e.innerText.substring(0, e.innerText.length - 2).toLowerCase());
   }
 
+  useEffect(() => {
+    (async () => {
+      const [res1, res2] = await Promise.all([
+        fetch(`http://seas.elte.hu/cube/index.pl?s=${escape(lastWord)}&fullw=on&invr=on&goal=on`),
+        fetch(`http://seas.elte.hu/cube/index.pl?s=${escape(lastWord)}&fullw=on&invr=on&goal=on&thop=on`)
+      ]);
+  
+      const [html1, html2] = await Promise.all([
+        getTextFromStream(res1.body), getTextFromStream(res2.body)
+      ]);
+
+      let [none, thop] = [getIPAs(html1), getIPAs(html2)];
+  
+      if (none.length === 0) {
+        setResults(["no results"]);
+        return () => {};
+      }
+      
+      thop.forEach((e, i) => {
+        [...e].forEach((c, index) => {
+          if (c === "ʰ") {
+            none[i] = none[i].substring(0, index) + "ʰ" + thinSpace
+            + none[i].substring(index + 1);
+          }
+        });
+      });
+      
+      const wordList = getWords(html1);
+      none = none.filter((e, i) => wordList[i] === lastWord.trim().toLowerCase());
+      none = none.map(e => {
+        return e.split(thinSpace).map(ele => {
+          if (ele.length > 1) {
+            if (ele.length == 2 && ele[1] !== accent && ele[1] !== "ː" && ele[1] !== "ʰ") {
+              return ele[0] + tie + thinSpace + ele[1];
+            }
+            else if (ele.length == 3 && ele[2] !== "ː") {
+              return ele.substring(0, 2) + tie + thinSpace + ele[2];
+            }
+          }
+          return ele;
+        }).join(thinSpace);
+      });
+  
+      setResults(none.length ? [...new Set(none)] : ["no results"]);
+    })();
+  }, [lastWord]);
+
   return (
     <div className="App">
       <form onSubmit={async e => {
         e.preventDefault();
 
+        setLastWord(field);
         setResults(["searching"]);
-
-        const [res1, res2] = await Promise.all([
-          fetch(`http://seas.elte.hu/cube/index.pl?s=${escape(field)}&fullw=on&invr=on&goal=on`),
-          fetch(`http://seas.elte.hu/cube/index.pl?s=${escape(field)}&fullw=on&invr=on&goal=on&thop=on`)
-        ]);
-
-        const [html1, html2] = await Promise.all([
-          getTextFromStream(res1.body), getTextFromStream(res2.body)
-        ]);
-
-        let [none, thop] = [getIPAs(html1), getIPAs(html2)];
-
-        if (none.length === 0) {
-          setResults(["no results"]);
-          return;
-        }
-        
-        thop.forEach((e, i) => {
-          [...e].forEach((c, index) => {
-            if (c === "ʰ") {
-              none[i] = none[i].substring(0, index) + "ʰ" + thinSpace
-              + none[i].substring(index + 1);
-            }
-          });
-        });
-        
-        const wordList = getWords(html1);
-        none = none.filter((e, i) => wordList[i] === field.trim().toLowerCase());
-        none = none.map(e => {
-          return e.split(thinSpace).map(ele => {
-            if (ele.length > 1) {
-              if (ele.length == 2 && ele[1] !== accent && ele[1] !== "ː" && ele[1] !== "ʰ") {
-                return ele[0] + tie + thinSpace + ele[1];
-              }
-              else if (ele.length == 3 && ele[2] !== "ː") {
-                return ele.substring(0, 2) + tie + thinSpace + ele[2];
-              }
-            }
-            return ele;
-          }).join(thinSpace);
-        });
-
-        setResults(none.length ? [...new Set(none)] : ["no results"]);
       }}>
         <input type="search" value={field} onChange={e => setField(e.target.value)} />
         <input type="submit" value="search" />
